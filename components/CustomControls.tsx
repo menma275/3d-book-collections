f35f5d37e5d95ddf813fa7af64118611746ef4bd
menma275 <ohsusie9@gmail.com>
2025-06-24 14:52:54 +0900

    orbitcontrolsの型を追加

--- components/CustomControls.tsx
+++ components/CustomControls.tsx
@@ -2,20 +2,21 @@ import * as THREE from "three";
 import { useRef, useEffect } from "react";
 import { useThree } from "@react-three/fiber";
 import { OrbitControls } from "@react-three/drei";
+import { OrbitControls as OrbitControlsImpl } from "three-stdlib";
 
 export default function CustomControls(): React.ReactElement {
   const { camera, gl } = useThree();
-  const controls = useRef<THREE.OrbitControls>(null);
+  const controls = useRef<OrbitControlsImpl>(null);
 
   useEffect(() => {
     const handleWheel = (event: WheelEvent) => {
       const deltaY = event.deltaY * 0.01;
-      // if (camera.position.z + deltaY < 20 && camera.position.z + deltaY > -10) {
       const offset = new THREE.Vector3();
-      offset.copy(camera.position).sub(controls.current.target);
-      camera.position.z += deltaY;
-      controls.current.target.z += deltaY;
-      // }
+      if (controls.current) {
+        offset.copy(camera.position).sub(controls.current.target);
+        camera.position.z += deltaY;
+        controls.current.target.z += deltaY;
+      }
     };
 
     gl.domElement.addEventListener("wheel", handleWheel);
