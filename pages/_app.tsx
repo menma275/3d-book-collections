9aeb169aae4e1372958a6579baf3eb8feba85657
menma275 <ohsusie9@gmail.com>
2025-06-24 15:23:45 +0900

    opg追加

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,16 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <title>3D Book Collections</title>
+        <meta property="og:description" content="Explore your books in 3D!" />
+        <meta property="og:image" content="/ogp.png" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
