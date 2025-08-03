ce9b6e3d5bfbe8596b9eb6292d68f14828b787df
menma275 <ohsusie9@gmail.com>
2025-08-03 21:49:43 +0900

    ogp設定を変更

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,9 +6,24 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
+        {/* Primary Meta Tags */}
         <title>3D Book Collections</title>
+        <meta name="title" content="3D Book Collections" />
+        <meta name="description" content="Explore your books in 3D!" />
+
+        {/* Open Graph / Facebook */}
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://3d-book-collections.vercel.app/" />
+        <meta property="og:title" content="3D Book Collections" />
         <meta property="og:description" content="Explore your books in 3D!" />
-        <meta property="og:image" content="https://3d-book-collections-krot.vercel.app/ogp.png" />
+        <meta property="og:image" content="https://3d-book-collections.vercel.app/ogp.png" />
+
+        {/* Twitter */}
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:url" content="https://3d-book-collections.vercel.app/" />
+        <meta name="twitter:title" content="3D Book Collections" />
+        <meta name="twitter:description" content="Explore your books in 3D!" />
+        <meta name="twitter:image" content="https://3d-book-collections.vercel.app/ogp.png" />
       </Head>
       <Component {...pageProps} />
     </>
