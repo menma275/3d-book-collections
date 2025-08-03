9ca28a2b6dbb1a22b262707ffb6de6d23de4f907
menma275 <ohsusie9@gmail.com>
2025-08-03 21:43:02 +0900

    静的データの配置場所変更

--- utils/staticBooks.ts
+++ utils/staticBooks.ts
@@ -1,10 +1,172 @@
 import { useEffect, useState } from "react";
 import { getBookInfos } from "../utils/book";
-import { staticData } from "../utils/staticData";
 import type { BookInfo, AdditionalBookInfo, MergedBookInfo } from "../types/types";
 
 const CACHE_KEY = "books";
 const CACHE_DURATION = 60 * 60 * 1000;
+
+const shinsho = [
+  {
+    "isbn": "9784121027566",
+    "size": {
+      "width": 220,
+      "height": 440,
+      "depth": 30
+    }
+  },
+  {
+    "isbn": "9784022952387",
+    "size": {
+      "width": 220,
+      "height": 440,
+      "depth": 30
+    }
+  },
+  {
+    "isbn": "9784480684523",
+    "size": {
+      "width": 220,
+      "height": 440,
+      "depth": 30
+    }
+  },
+  {
+    "isbn": "9784140887004",
+    "size": {
+      "width": 220,
+      "height": 440,
+      "depth": 30
+    }
+  },
+  {
+    "isbn": "9784065308899",
+    "size": {
+      "width": 220,
+      "height": 440,
+      "depth": 30
+    }
+  },
+  {
+    "isbn": "9784087212747",
+    "size": {
+      "width": 220,
+      "height": 440,
+      "depth": 30
+    }
+  },
+  {
+    "isbn": "9784065321294",
+    "size": {
+      "width": 220,
+      "height": 440,
+      "depth": 30
+    }
+  },
+  {
+    "isbn": "9784480075284",
+    "size": {
+      "width": 220,
+      "height": 440,
+      "depth": 30
+    }
+  },
+  {
+    "isbn": "9784004319559",
+    "size": {
+      "width": 220,
+      "height": 440,
+      "depth": 30
+    }
+  },
+  {
+    "isbn": "9784065336915",
+    "size": {
+      "width": 220,
+      "height": 440,
+      "depth": 30
+    }
+  },
+  {
+    "isbn": "9784004319948",
+    "size": {
+      "width": 220,
+      "height": 440,
+      "depth": 30
+    }
+  },
+  {
+    "isbn": "9784106109911",
+    "size": {
+      "width": 220,
+      "height": 440,
+      "depth": 30
+    }
+  },
+  {
+    "isbn": "9784480075789",
+    "size": {
+      "width": 220,
+      "height": 440,
+      "depth": 30
+    }
+  },
+  {
+    "isbn": "9784121027689",
+    "size": {
+      "width": 220,
+      "height": 440,
+      "depth": 30
+    }
+  },
+  {
+    "isbn": "9784121027429",
+    "size": {
+      "width": 220,
+      "height": 440,
+      "depth": 30
+    }
+  },
+  {
+    "isbn": "9784121027733",
+    "size": {
+      "width": 220,
+      "height": 440,
+      "depth": 30
+    }
+  },
+  {
+    "isbn": "9784140886908",
+    "size": {
+      "width": 220,
+      "height": 440,
+      "depth": 30
+    }
+  },
+  {
+    "isbn": "9784309631677",
+    "size": {
+      "width": 220,
+      "height": 440,
+      "depth": 30
+    }
+  },
+  {
+    "isbn": "9784334046477",
+    "size": {
+      "width": 220,
+      "height": 440,
+      "depth": 30
+    }
+  },
+  {
+    "isbn": "9784480075840",
+    "size": {
+      "width": 220,
+      "height": 440,
+      "depth": 30
+    }
+  }
+]
 
 const useStaticBooks = () => {
   const [books, setBooks] = useState<MergedBookInfo[] | null>(null);
@@ -29,7 +191,7 @@ const useStaticBooks = () => {
   const [bookInfos, setBookInfos] = useState<BookInfo[]>([]);
   useEffect(() => {
     if (useCache) return;
-    const infos = staticData();
+    const infos = shinsho;
     setBookInfos(infos);
   }, [useCache]);
 
